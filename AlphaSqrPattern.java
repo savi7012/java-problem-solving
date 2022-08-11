02bc511c315f1673e277f2223400038bce4362ff
savi7012 <vishalsaini72@gmail.com>
2022-08-11 19:19:24 +0530

    AlphaSqrPattern

---
+++ AlphaSqrPattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/* 
+
+ n = 3 
+
+ C C C
+ B B B
+ A A A
+
+A A A
+n = 3  -> 65+3 = 68,67,66,65
+
+ */
+
+public class AlphaSqrPattern {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       for (int i= n + 65; i >= 65; i--){
+       displayAlphabet(n, i);
+       System.out.println();
+       }
+    }
+
+    static void displayAlphabet(int n, int code){
+        for(int i =1; i <= n; i ++){
+            System.out.print((char)code + " ");
+        }
+    }
+}
